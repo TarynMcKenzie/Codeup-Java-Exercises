e8036b1a531a02328484644fbf6924bde3e1d222
Taryn McKenzie <taryn.m.mckenzie@gmail.com>
2020-03-19 15:14:44 -0500

    Completed the exercise for Syntax, Types and Variables

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,13 +1,16 @@
 
+
+//__________________________________EXERCISE 1______________________________________
+
 //  TODO: Inside your project, right click on the src directory and select 'New -> Java Class'.
 //  TODO: Name this class HelloWorld.
 //  TODO: Create a program that outputs "Hello, World!" to the console.
 
-        public class HelloWorld {
-            public static void main (String[] args){
-                System.out.print("Hello World!");
-            }
-        }
+//        public class HelloWorld {
+//            public static void main (String[] args){
+//                System.out.print("Hello World!");
+//            }
+//        }
 
 //  TODO: Run the application using the "Run" menubar item in IntelliJ (Note there are many ways to run a Java file). IntelliJ will first compile your HelloWorld.java file, then execute the compiled code.
         // command + option + r
@@ -63,3 +66,190 @@
             // taryn.mckenzie@Taryns-MacBook-Pro src % java HelloWorld
             // output:
                 // Hello World!%
+
+
+
+//__________________________________EXERCISE 2______________________________________
+
+// TODO: You should complete all of the following inside of your main method from the previous exercise. After each step, compile and run your code.
+
+//        TODO: Create an int variable named myFavoriteNumber and assign your favorite number to it, then print it out to the console.
+
+//                public class HelloWorld {
+//                    public static void main (String[] args){
+//                        int myFavoriteNumber = 21;
+//                        System.out.print(myFavoriteNumber);
+//                    }
+//                }
+
+//        TODO: Create a String variable named myString and assign a string value to it, then print the variable out to the console.
+
+//                public class HelloWorld {
+//                    public static void main (String[] args){
+//                        String myString = "Hello"; // double quote is for a string of words
+//                        System.out.print(myString);
+//                    }
+//                }
+
+//        TODO: Change your code to assign a character value to myString. What do you notice?
+
+//                public class HelloWorld {
+//                    public static void main (String[] args){
+//                        String myString = 'Hello'; //single quote is a character string
+//                        System.out.print(myString);
+//                    }
+//                }
+
+//        TODO: Change your code to assign the value 3.14159 to myString. What happens?
+
+//                public class HelloWorld {
+//                    public static void main (String[] args){
+//                        String myString = "3.14159";
+//                        System.out.print(myString);
+//                    }
+//                }
+
+//        TODO: Declare an long variable named myNumber, but do not assign anything to it. Next try to print out myNumber to the console. What happens?
+
+//                public class HelloWorld {
+//                    public static void main (String[] args){
+//                        long myNumber;
+//                        System.out.print(myNumber);
+//                    }
+//                }
+//
+                //Error:(114, 42) java: variable myNumber might not have been initialized
+
+//        TODO: Change your code to assign the value 3.14 to myNumber. What do you notice?
+
+//                public class HelloWorld {
+//                    public static void main (String[] args){
+//                        long myNumber = 3.14;
+//                        System.out.print(myNumber);
+//                    }
+//                }
+
+                //Error:(124, 41) java: incompatible types: double cannot be converted to java.lang.Long
+
+//        TODO: Change your code to assign the value 123L (Note the 'L' at the end) to myNumber.
+
+//                public class HelloWorld {
+//                    public static void main (String[] args){
+//                        long myNumber = 123L;
+//                        System.out.print(myNumber);
+//                    }
+//                }
+
+//        TODO: Change your code to assign the value 123 to myNumber.
+
+//                public class HelloWorld {
+//                    public static void main (String[] args){
+//                        long myNumber = 123;
+//                        System.out.print(myNumber);
+//                    }
+//                }
+
+                //Error:(144, 41) java: incompatible types: int cannot be converted to java.lang.Long
+
+//        TODO: Why does assigning the value 3.14 to a variable declared as a long not compile, but assigning an integer value does?
+
+            // 3.14 needs to be a float because of the decimal?!?!
+
+//        TODO: Change your code to declare myNumber as a float. Assign the value 3.14 to it. What happens? What are two ways we could fix this?
+
+//                public class HelloWorld {
+//                    public static void main (String[] args){
+//                        double myNumber = 3.14;
+//                        System.out.print(myNumber);
+//                    }
+//                }
+
+//        TODO: Copy and paste the following code blocks one at a time and execute them
+
+//                public class HelloWorld {
+//                    public static void main (String[] args){
+//
+//                //  int x = 5;
+//                //  System.out.println(x++);
+//                //  System.out.println(x);
+//
+//                //  int x = 5;
+//                //  System.out.println(++x);
+//                //  System.out.println(x);
+//
+//                    }
+//                }
+
+//        TODO: What is the difference between the above code blocks? Explain why the code outputs what it does.
+
+                // 1. The incrementation happens after the value of x is printed
+                // 2. The incrementation happens before the value of x is printed
+
+//        TODO: Try to create a variable named class. What happens?
+
+//                public class HelloWorld {
+//                    public static void main (String[] args){
+//                        double class;
+//                    }
+//                }
+
+                //Error: using a reserved keyword
+
+//        TODO: Object is the most generic type in Java. You can assign any value to a variable of type Object. What do you think will happen when the following code is run?
+//        TODO: Copy and paste the code above and then run it. Does the result match with your expectation?
+
+//                public class HelloWorld {
+//                    public static void main (String[] args){
+//
+//                        String theNumberThree = "three";
+//                        Object o = theNumberThree;
+//                        int three = (int) o;
+//
+//                    }
+//                }
+
+                //Exception in thread "main" java.lang.ClassCastException: class java.lang.String cannot be cast to class java.lang.Integer (java.lang.String and java.lang.Integer are in module java.base of loader 'bootstrap')
+//	at HelloWorld.main(HelloWorld.java:202)
+
+
+//        TODO: How is the above example different from the code block below?
+
+//                public class HelloWorld {
+//                    public static void main (String[] args){
+//
+//                         int three = (int) "three"; //trying to cast as a string
+//
+//                    }
+//                }
+
+//        TODO: What are the two different types of errors we are observing?
+
+            //implicit and explicit?
+
+//        TODO: Rewrite the following expressions using the relevant shorthand assignment operators:
+
+                //        int x = 4;
+                //        x = x + 5;
+
+                //        x += 5;
+
+                //        int x = 3;
+                //        int y = 4;
+                //        y = y * x;
+
+                //        y *= x
+
+                //        int x = 10;
+                //        int y = 2;
+                //        x = x / y;
+                //        y = y - x;
+
+                //        x /= y
+                //        y -= x
+
+
+//        TODO: What happens if you assign a value to a numerical variable that is larger (or smaller) than the type can hold? What happens if you increment a numeric variable past the type's capacity?
+                // Hint: Integer.MAX_VALUE is a class constant (we'll learn more about these later) that holds the maximum value for the int type.
+
+                // You run the risk of losing data because the cast is small than what you are trying to catch.
+
