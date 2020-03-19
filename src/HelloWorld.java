0ebda78eac8d75d678e8d9cc365e48b4941a9dd9
Taryn McKenzie <taryn.m.mckenzie@gmail.com>
2020-03-19 10:49:06 -0500

    Complete the java exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,27 @@
-public class HelloWorld {
-    public static void main (String[] args){
-        System.out.println("Hello World!");
-    }
-}
+
+//  TODO: Inside your project, right click on the src directory and select 'New -> Java Class'.
+//  TODO: Name this class HelloWorld.
+//  TODO: Create a program that outputs "Hello, World!" to the console.
+
+        public class HelloWorld {
+            public static void main (String[] args){
+                System.out.print("Hello World!");
+            }
+        }
+
+//  TODO: Run the application using the "Run" menubar item in IntelliJ (Note there are many ways to run a Java file). IntelliJ will first compile your HelloWorld.java file, then execute the compiled code.
+        // command + option + r
+//  TODO: Look at your project structure. You should notice a new directory named out. This is where IntelliJ puts the compiled Java files, to keep them separate from the source code.
+
+//  TODO: Try renaming the main method. What happens when you try to run the program?
+//
+//      When main is changed the system runs an error the gives your the expected line syntax :
+        //          "Error: Main method not found in class HelloWorld, please define the main method as:
+        //          public static void main(String[] args)
+        //          or a JavaFX application class must extend javafx.application.Application"
+
+//  TODO: System.out.println is used to output information to the console with a newline. System.out.print can be used to output information without a newline. Refactor your code to use two System.out.print statements without changing the output.
+        // println is a new line for each system print output
+        // print put each system print output on the same line
+
+
