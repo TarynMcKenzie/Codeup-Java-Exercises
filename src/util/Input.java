29a3ef74c7ac5a74e12a02bf560f11b7a6ee95b1
Taryn McKenzie <taryn.m.mckenzie@gmail.com>
2020-03-25 17:12:32 -0500

    Exercise not complete, pushing for the evening

--- src/util/Input.java
+++ src/util/Input.java
@@ -8,16 +8,17 @@ public class Input {
     // TODO: Create a package inside of src named util.
     //       Inside of util, create a class named Input that has a private property named scanner.
     //       When an instance of this object is created, the scanner property should be set to a new instance of the Scanner class.
+
     private Scanner scanner;
 
-    scanner =  new Scanner(System.in);
-
-    String getString();
-    double getDouble();
+    private Scanner scanner() { // private property named scanner
+        return scanner =  new Scanner(System.in); // new instance of the Scanner class
+    }
 
     // TODO: The yesNo method should return true if the user enters y, yes, or variants thereof, and false otherwise.
     boolean yesNo(){
-        return ;
+        String scannerInput = scanner.nextLine();
+        if (scannerInput.equals('y')) return true;
     }
 
     // TODO: The getInt(int min, int max) method should keep prompting the user for input until they give an integer within the min and max.
@@ -35,9 +36,9 @@ public class Input {
 
         public static void main(String[] args) {
 
-            boolean yesNo();
-            int getInt();
-            double getDouble();
+            yesNo();
+            getInt();
+            getDouble();
 
         }
 
