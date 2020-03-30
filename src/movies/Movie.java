f82fb5306a95c048726cd57385ffe7bccf54d747
Taryn McKenzie <taryn.m.mckenzie@gmail.com>
2020-03-30 11:19:48 -0500

    Began work on exercise 3/3 for Arrays exercise. Currently following the instructors walkthrough of exercises

---
+++ src/movies/Movie.java
@@ -0,0 +1,32 @@
+package movies;
+
+public class Movie {
+
+    // We are going to build an application that keeps track of movies and displays them by category.
+
+    // TODO: Inside of src, create a directory named movies. Create all of the classes described below in this directory.
+    // TODO: Create a class named Movie. It should have private fields for name and category, and a constructor that sets both of these.
+    //       Create methods to access these properties and change them (getters and setters).
+
+    private String name;
+    private String category;
+
+    //--------------Get + SET (CATEGORY)------------------
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    //--------------Get + SET (Name)------------------
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
---
+++ src/movies/MoviesApplication.java
@@ -0,0 +1,32 @@
+package movies;
+
+public class MoviesApplication {
+
+    // TODO: Create a class named MoviesApplication that has a main method.
+    public static void main(String[] args) {
+
+        // TODO: Give the user a list of options for viewing all the movies or viewing movies by category.
+
+        // TODO: Use your Input class to get input from the user, and display information based on their choice. (Remember to import your Input class)
+        // If a category is selected, only movies from that category should be displayed.
+        // Your application should continue to run until the user chooses to exit.
+        // Sample Output
+
+
+        // What would you like to do?
+
+        // 0 - exit
+        // 1 - view all movies
+        // 2 - view movies in the animated category
+        // 3 - view movies in the drama category
+        // 4 - view movies in the horror category
+        // 5 - view movies in the scifi category
+
+        //Enter your choice: 1
+
+        //Frankenstein -- horror
+        //Goodfellas -- drama
+        //Pulp Fiction -- drama
+        //...
+    }
+}
