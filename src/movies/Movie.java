6af06f3cbaf51aaba723169bf238d718b7926829
Taryn McKenzie <taryn.m.mckenzie@gmail.com>
2020-03-30 11:42:59 -0500

    Instructor walkthrough completed. Still working on the movies application exercise (3/3). New lesson on polymorphism began

--- src/movies/Movie.java
+++ src/movies/Movie.java
@@ -5,11 +5,21 @@ public class Movie {
     // We are going to build an application that keeps track of movies and displays them by category.
 
     // TODO: Inside of src, create a directory named movies. Create all of the classes described below in this directory.
-    // TODO: Create a class named Movie. It should have private fields for name and category, and a constructor that sets both of these.
+    // TODO: Create a class named Movie.
+    //       It should have private fields for name and category, and a constructor that sets both of these.
     //       Create methods to access these properties and change them (getters and setters).
 
+    //--------------INITIALIZE------------------
     private String name;
     private String category;
+
+
+    //--------------MOVIE CONSTRUCTOR------------------
+    public Movie(String name, String category) {
+        this.name = name;
+        this.category = category;
+    }
+
 
     //--------------Get + SET (CATEGORY)------------------
     public String getCategory() {
--- src/movies/MoviesApplication.java
+++ src/movies/MoviesApplication.java
@@ -1,32 +1,75 @@
 package movies;
 
+import util.Input;
+
 public class MoviesApplication {
 
     // TODO: Create a class named MoviesApplication that has a main method.
     public static void main(String[] args) {
 
         // TODO: Give the user a list of options for viewing all the movies or viewing movies by category.
-
         // TODO: Use your Input class to get input from the user, and display information based on their choice. (Remember to import your Input class)
-        // If a category is selected, only movies from that category should be displayed.
-        // Your application should continue to run until the user chooses to exit.
-        // Sample Output
+                // If a category is selected, only movies from that category should be displayed.
+                // Your application should continue to run until the user chooses to exit.
+
+                // Sample Output
 
 
-        // What would you like to do?
+                // What would you like to do?
 
-        // 0 - exit
-        // 1 - view all movies
-        // 2 - view movies in the animated category
-        // 3 - view movies in the drama category
-        // 4 - view movies in the horror category
-        // 5 - view movies in the scifi category
+//                 0 - exit
+//                 1 - view all movies
+//                 2 - view movies in the animated category
+//                 3 - view movies in the drama category
+//                 4 - view movies in the horror category
+//                 5 - view movies in the scifi category
 
-        //Enter your choice: 1
+                //Enter your choice: 1
 
-        //Frankenstein -- horror
-        //Goodfellas -- drama
-        //Pulp Fiction -- drama
-        //...
+                //Frankenstein -- horror
+                //Goodfellas -- drama
+                //Pulp Fiction -- drama
+                //...
+
+        //--------------USER PROMPT------------------
+        System.out.println("What would you like to do?" +
+                "\t0 - exit\n" +
+                "\t1 - view all movies\n" +
+                "\t2 - view movies in the animated category\n" +
+                "\t3 - view movies in the drama category\n" +
+                "\t4 - view movies in the horror category\n" +
+                "\t5 - view movies in the scifi category");
+
+        //--------------NEW INPUT INSTANCE------------------
+        Input userInput = new Input();
+
+        //--------------CAPTURE USER INPUT------------------
+        int validate = userInput.getInt(0, 5);
+
+
+        //--------------CHECK USER INPUT------------------
+        switch (validate){
+            case 0:
+                System.out.println("exit");
+                break;
+            case 1:
+                System.out.println("View all Movies");
+                break;
+            case 2:
+                System.out.println("View movies in the animated category");
+                break;
+            case 3:
+                System.out.println("View movies in the drama category");
+                break;
+            case 4:
+                System.out.println("View movies in the horror category");
+                break;
+            case 5:
+                System.out.println("View movies int he scifi category");
+                break;
+            default:
+                break;
+        }
+
     }
 }
