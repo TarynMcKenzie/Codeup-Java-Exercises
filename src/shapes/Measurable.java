32b0027b13f4f4424880768bb4731953e8dc94c9
Taryn McKenzie <taryn.m.mckenzie@gmail.com>
2020-03-31 16:02:32 -0500

    Walkthrough completed by instructor, Trant.

--- src/shapes/Measurable.java
+++ src/shapes/Measurable.java
@@ -5,4 +5,6 @@ public interface Measurable {
     // TODO: Inside of the shapes directory, create a Measurable interface with the following methods:
     double getPerimeter(); // return the perimeter of the shape
     double getArea(); // return the area of the shape
+
+    // Interface variables are public
 }
